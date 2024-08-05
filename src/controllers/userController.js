364a35bc6bbecb550a4ed2879d3b9863c443ad55
Matheus Henrique Lourenço Bernardo <matheus.bernardo@inatel.br>
2024-08-05 11:22:05 -0300

    :sparkles: feat: add-columnPassword + bycript

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,11 +1,13 @@
 const User = require('../models/userModel');
 const UserType = require('../models/userTypeModel');
+const bcrypt = require('bcrypt');
+
 
 const createUser = async (req, res) => {
   try {
-    const { firstName, lastName, email, userTypeId } = req.body;
+    const { firstName, lastName, email, userTypeId, password:plainPassword } = req.body;
 
-    if (!firstName || !lastName || !email || !userTypeId) {
+    if (!firstName || !lastName || !email || !userTypeId || !plainPassword)   {
       return res.status(400).json({ error: 'Todos os campos são obrigatórios.' });
     }
 
@@ -14,11 +16,21 @@ const createUser = async (req, res) => {
       return res.status(400).json({ error: 'Tipo de usuário inválido.' });
     }
 
+    try {
+      const saltRounds = 10;
+      const hashedPassword = await bcrypt.hash(plainPassword, saltRounds);
+      password = hashedPassword;
+    } catch (error) {
+      console.error("Erro ao criptografar:", error);
+      return res.status(500).json({ error: 'Erro ao criptografar a senha.' });
+    }
+
     const newUser = await User.create({
       firstName,
       lastName,
       email,
-      userTypeId
+      userTypeId,
+      password
     });
 
     return res.status(201).json(newUser);
