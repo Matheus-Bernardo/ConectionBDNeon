f341664a3b3b2089ae47033c1194910c457a9f02
Matheus Henrique Lourenço Bernardo <matheus.bernardo@inatel.br>
2024-08-05 12:36:24 -0300

    :sparkles: feat: login

--- src/app.js
+++ src/app.js
@@ -2,10 +2,12 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const sequelize = require('./db/conecctionBD');
 const userRoutes = require('./routes/userRoutes');
+const loginRoutes = require('./routes/loginRoutes')
 
 const app = express();
 app.use(bodyParser.json());
 app.use('/users', userRoutes);
+app.use('/login',loginRoutes)
 
 sequelize.sync()
   .then(result => {
